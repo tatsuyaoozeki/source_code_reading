6efcf004553875073ba1d3b757a80394cd5f44f4
tatsuya oozeki <tasuya6281@icloud.com>
2019-08-30 10:40:45 +0900

    3分間おきにメッセージが来るように設定

--- sample.rb
+++ sample.rb
@@ -5,6 +5,7 @@ require 'dotenv/load'
 
 module Clockwork
   sample = SlackNotify::Client.new(webhook_url:ENV['key']) #事前準備で取得したWebhook URLを.envで見えないようにする
+    every(3.minites, sample) do
       sample.notify('Hello')
     end
 end
